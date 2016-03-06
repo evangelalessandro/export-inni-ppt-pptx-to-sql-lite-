2008bfc0b2dbba147de05ec60897888bf9dc6b93
evangelalessandro <evangel.alessandro@gmail.com>
2016-03-06 20:47:38 +0100

    Versione 1, riesco a leggere un pptx e esportare il testo

---
+++ export ppt to txt/ExportJob.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Text;
+
+namespace export_ppt_to_txt
+{
+
+    public class ExportJob :IDisposable
+    {
+        public String method = "";
+        public String file = "";
+        public String options = "";
+        //ExportingDialog ed;
+
+        //public void run(Form f)
+        //{
+        //    ed = new ExportingDialog();
+        //    ed.job = this;
+        //    ed.Show(f);
+        //}
+
+        String getTempDir()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+
+        Microsoft.Office.Interop.PowerPoint.Application openPowerPoint()
+        {
+            Microsoft.Office.Interop.PowerPoint.Application powerPoint = new Microsoft.Office.Interop.PowerPoint.Application();
+            foreach (Presentation ppt in powerPoint.Presentations)
+            {
+                ppt.Close();
+            }
+            powerPoint.Presentations.Open(file);
+            return powerPoint;
+        }
+
+        public void runInForm(string myFile)
+        {
+            file = myFile;
+            System.Diagnostics.Debug.WriteLine("Exporting " + file + "...");
+            String tempDir = getTempDir();
+            System.Diagnostics.Debug.WriteLine("Tempoary Directory: " + tempDir);
+
+                 Package zipPresentation = null;
+                if (options.Contains("exportNotes"))
+                {
+                    String tmpFile = Path.Combine(tempDir, "pres.zip");
+                    File.Copy(file, tmpFile);
+                    zipPresentation = ZipPackage.Open(new FileStream(tmpFile, FileMode.Open));
+                }
+
+                Microsoft.Office.Interop.PowerPoint.Application powerPoint = openPowerPoint();
+                System.Diagnostics.Debug.WriteLine("PowerPoint should have loaded document");
+                System.Diagnostics.Debug.WriteLine("Exporting as handouts...");
+                Presentation toExport = powerPoint.Presentations[1];
+
+                //System.Diagnostics.Debug.WriteLine("Opening Publisher Template");
+                //Microsoft.Office.Interop.Publisher.Application publisher = new Microsoft.Office.Interop.Publisher.Application();
+
+                //String pubFile = Path.Combine(tempDir, "out.pub");
+                //File.Copy(Path.Combine(
+                //    Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath),
+                //    "Templates",
+                //    "Handout-3P.pub" // TODO: Add option for different templates
+                //), pubFile);
+                //Document publisherDoc = publisher.Open(pubFile);
+                //publisher.ActiveWindow.Visible = true;
+
+
+                int slideWidth = (int)toExport.PageSetup.SlideWidth;
+                int slideHeight = (int)toExport.PageSetup.SlideHeight;
+                int slideCount = 1, onPage = 1;
+                //publisherDoc.Pages[onPage].Duplicate();
+
+                foreach (_Slide slide in toExport.Slides)
+                {
+                    if (slideCount == 4)  // End of Page but only create a new one, if there is more!
+                    {
+                        //publisherDoc.Pages[onPage + 1].Duplicate();
+                        onPage += 1;
+                        slideCount = 1;
+                    }
+                
+                    System.Diagnostics.Debug.WriteLine("Exporting slide #" + slide.SlideNumber);
+                     
+                    String notes = "";
+                    //if (options.Contains("exportNotes"))
+                    //{
+                    //    // I HATE YOU POWERPOINT!
+                    //    Uri partUriResource = PackUriHelper.CreatePartUri(
+                    //          new Uri(@"ppt/notesSlides/notesSlide" + slide.SlideNumber + ".xml", UriKind.Relative));
+                    //    if (zipPresentation.PartExists(partUriResource))
+                    //    {
+                    //        Stream noteStream = zipPresentation.GetPart(partUriResource).GetStream(FileMode.Open, FileAccess.Read);
+                    //        StreamReader nSR = new StreamReader(noteStream);
+
+                    //        // Strip crap
+                    //        notes = nSR.ReadToEnd();
+                    //        // Chop out slide numbers. I don't have a clue really :')
+                    //        int s = notes.IndexOf("<p:txBody>");
+                    //        if (s != -1)
+                    //        {
+                    //            notes = notes.Substring(s, notes.IndexOf("</p:txBody>") - s);
+                    //            notes = (new Regex("<[^>]*>")).Replace(notes, "");
+                    //        }
+
+                    //        nSR.Close();
+                    //        noteStream.Close();
+                    //    }
+                    //}
+
+
+                    //foreach (Microsoft.Office.Interop.Publisher.Shape shape in publisherDoc.Pages[onPage].Shapes)
+                    //{
+                    //    if (shapeHasTag(shape, "Picture " + slideCount))
+                    //    {
+                    //        shape.Fill.UserPicture(fname);
+                    //    }
+                    //    else if (shapeHasTag(shape, "Text " + slideCount))
+                    //    {
+                    //        shape.TextFrame.AutoFitText = PbTextAutoFitType.pbTextAutoFitBestFit;
+                    //        shape.TextFrame.TextRange.Text = notes;
+                    //    }
+                    //}
+
+                    slideCount += 1;
+                }
+
+                //publisherDoc.Pages[publisherDoc.Pages.Count].Delete();
+                toExport.Close();
+                System.Diagnostics.Debug.WriteLine("Exported as handouts :)");
+            
+            //ed.finishJob("");
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // Per rilevare chiamate ridondanti
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminare lo stato gestito (oggetti gestiti).
+                }
+
+                // TODO: liberare risorse non gestite (oggetti non gestiti) ed eseguire sotto l'override di un finalizzatore.
+                // TODO: impostare campi di grandi dimensioni su Null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: eseguire l'override di un finalizzatore solo se Dispose(bool disposing) include il codice per liberare risorse non gestite.
+        // ~ExportJob() {
+        //   // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+        //   Dispose(false);
+        // }
+
+        // Questo codice viene aggiunto per implementare in modo corretto il criterio Disposable.
+        public void Dispose()
+        {
+            // Non modificare questo codice. Inserire il codice di pulizia in Dispose(bool disposing) sopra.
+            Dispose(true);
+            // TODO: rimuovere il commento dalla riga seguente se è stato eseguito l'override del finalizzatore.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        //private bool shapeHasTag(Microsoft.Office.Interop.Publisher.Shape s, string p)
+        //{
+        //    foreach (Tag t in s.Tags)
+        //    {
+        //        if (t.Name == "PPE" && t.Value == p)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+     
+    
+}
+}
+
---
+++ export ppt to txt/Form1.cs
@@ -0,0 +1,140 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace export_ppt_to_txt
+{
+    public partial class Form1 : Form
+    {
+        public string filepptx = @"C:\Users\MioAle\Downloads\innario_completo\innario completo\298. Chi potrà dir qual sia la gloria.pptx";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var provaExport = new ExportJob())
+            {
+                provaExport.runInForm(filepptx);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numberOfSlides = CountSlides(filepptx);
+            System.Console.WriteLine("Number of slides = {0}", numberOfSlides);
+            string slideText;
+            for (int i = 0; i < numberOfSlides; i++)
+            {
+                GetSlideIdAndText(out slideText, filepptx, i);
+                System.Console.WriteLine("Slide #{0} contains: {1}", i + 1, slideText);
+
+                textBox1.Text = textBox1.Text + Environment.NewLine + Environment.NewLine + slideText;
+            }
+        }
+
+        public static int CountSlides(string presentationFile)
+        {
+            // Open the presentation as read-only.
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(presentationFile, false))
+            {
+                // Pass the presentation to the next CountSlides method
+                // and return the slide count.
+                return CountSlides(presentationDocument);
+            }
+        }
+
+        // Count the slides in the presentation.
+        public static int CountSlides(PresentationDocument presentationDocument)
+        {
+            // Check for a null document object.
+            if (presentationDocument == null)
+            {
+                throw new ArgumentNullException("presentationDocument");
+            }
+
+            int slidesCount = 0;
+
+            // Get the presentation part of document.
+            PresentationPart presentationPart = presentationDocument.PresentationPart;
+            // Get the slide count from the SlideParts.
+            if (presentationPart != null)
+            {
+                slidesCount = presentationPart.SlideParts.Count();
+            }
+            // Return the slide count to the previous method.
+            return slidesCount;
+        }
+
+        public static void GetSlideIdAndText(out string sldText, string docName, int index)
+        {
+            using (PresentationDocument ppt = PresentationDocument.Open(docName, false))
+            {
+                // Get the relationship ID of the first slide.
+                PresentationPart part = ppt.PresentationPart;
+                OpenXmlElementList slideIds = part.Presentation.SlideIdList.ChildElements;
+
+                string relId = (slideIds[index] as SlideId).RelationshipId;
+
+                // Get the slide part from the relationship ID.
+                SlidePart slide = (SlidePart)part.GetPartById(relId);
+
+                // Build a StringBuilder object.
+                StringBuilder paragraphText = new StringBuilder();
+
+                //// Get the inner text of the slide:
+                //var texts = slide.Slide.Descendants().Where(a=>a.InnerText!="").Select(a=>a.InnerText).ToList();
+                //foreach (var text in texts)
+                //{
+                //    paragraphText.Append(text);
+                //}
+
+                var xmlText = slide.Slide.InnerXml.Replace("\t", "").Replace("\t", "");
+                while (true)
+                {
+                    int indexTExt = xmlText.IndexOf("<a:t>");
+                    if (indexTExt > -1)
+                    {
+
+
+                        xmlText = xmlText.Remove(0,indexTExt+ "<a:t>".Length);
+
+                        int indexEndTExt = xmlText.IndexOf("</a:t>");
+                        paragraphText.Append(xmlText.Substring(0, indexEndTExt));
+
+                        indexTExt = xmlText.IndexOf("<a:t>");
+                        int indexTExtbr = xmlText.IndexOf("</a:br>");
+                        if (indexTExtbr > -1)
+                        {
+                         
+                            if (indexTExtbr < indexTExt)
+                            {
+                                xmlText = xmlText.Remove(0,indexTExtbr+ "</a:br>".Length);
+                                paragraphText.AppendLine("");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        sldText = paragraphText.ToString();
+                        return;
+                    }
+                }
+
+                sldText = paragraphText.ToString();
+                return;
+            }
+        }
+    }
+}
